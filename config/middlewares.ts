5cbc07d5b6d5ae2768b023298c5a9903659302ec
Saboor Hakimi <saboor@aseelapp.com>
2023-03-03 20:43:55 +0430

    setup strapi security (CSP) settings to load images from cloudinary

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,6 +1,36 @@
-export default [
+module.exports = ({ env }) => [
   'strapi::errors',
-  'strapi::security',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'https:'],
+
+          // load images from anywhere
+          'img-src': [
+            "'self'",
+            'data:',
+            'https:',
+            'blob:',
+            'res.cloudinary.com',
+          ],
+          // load media from anywhere
+          'media-src': [
+            "'self'",
+            'data:',
+            'https:',
+            'blob:',
+            'res.cloudinary.com',
+          ],
+
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
+  // 'strapi::security',
   'strapi::cors',
   'strapi::poweredBy',
   'strapi::logger',
