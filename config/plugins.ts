638f5ebac10b41225b8f0e96caf28c33abd909b3
Saboor Hakimi <saboor@aseelapp.com>
2023-03-03 21:28:53 +0430

    setup sendgrid integration and testing that it's working

--- config/plugins.ts
+++ config/plugins.ts
@@ -2,11 +2,11 @@ module.exports = ({ env }) => ({
   // ...
   upload: {
     config: {
-      provider: "cloudinary",
+      provider: 'cloudinary',
       providerOptions: {
-        cloud_name: env("CLOUDINARY_NAME"),
-        api_key: env("CLOUDINARY_KEY"),
-        api_secret: env("CLOUDINARY_SECRET"),
+        cloud_name: env('CLOUDINARY_NAME'),
+        api_key: env('CLOUDINARY_KEY'),
+        api_secret: env('CLOUDINARY_SECRET'),
       },
       actionOptions: {
         upload: {},
@@ -15,5 +15,20 @@ module.exports = ({ env }) => ({
       },
     },
   },
+
+  // ...
+  email: {
+    config: {
+      provider: 'sendgrid',
+      providerOptions: {
+        apiKey: env('SENDGRID_API_KEY'),
+      },
+      settings: {
+        defaultFrom: env('SENDGRID_FROM'),
+        defaultReplyTo: env('SENDGRID_REPLY_TO'),
+      },
+    },
+  },
+
   // ...
 });
